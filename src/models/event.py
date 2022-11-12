17266e8556c20f6e4871d4094891f2d782ade621
gadnlino <guiavenas@gmail.com>
2022-11-12 17:42:40 -0300

    gerando arquivo com a lista de eventos bruta

--- src/models/event.py
+++ src/models/event.py
@@ -29,23 +29,9 @@ class Event:
             self.remaining_service_time = remaining_service_time
         else:
             self.remaining_service_time = None
-    
-    #Critérios
-    #1) menor tempo primeiro
-    #2) Partidas antes de chegadas
-    #3) Fila 1 antes do que a fila 2
-    # def __lt__(self, other):
-    #     return self.timestamp < other.timestamp or\
-    #          event_type_values[self.type] < event_type_values[other.type] or \
-    #             self.queue_number < other.queue_number
 
     def __lt__(self, other):
-        return self.timestamp < other.timestamp
-    
-    # def __le__(self, other):
-    #     return self.timestamp <= other.timestamp or\
-    #          event_type_values[self.type] < event_type_values[other.type] or\
-    #              self.queue_number < other.queue_number
+        return self.timestamp < other.timestamp   
 
     def __le__(self, other):
         return self.timestamp <= other.timestamp
--- src/simulator2.py
+++ src/simulator2.py
@@ -2,36 +2,48 @@ import bisect
 import heapq
 import json
 import os
+import csv
 
 import numpy as np
 
 from models.event import Event
 from models.event_type import EventType
 
+
 class Simulator:
 
     def __init__(self):
-        self.__event_q = []
+        self.__event_list_raw_file = './event_list_raw.csv'
 
-        self.__event_q_history = []
+        self.__event_list_raw = []
 
         self.__number_of_qs = 2
-
-        self.__waiting_qs = list(map(lambda _ : list(), range(0, self.__number_of_qs)))
 
         self.__utilization_pct = 0.99
 
         #tempo máximo de chegada de pessoas no sistema(tempo de funcionamento da loja)
-        self.__system_max_arrival_time = 100
+        self.__system_max_arrival_time = 1000
 
         self.__service_rate = 1
 
         self.__arrival_rate = 1 / (self.__utilization_pct *
                                    self.__service_rate)
 
+        self.__number_of_rounds = 3
+
+        self.__current_round = 0
+    
+    def __reset_simulation_variables(self):
+        #simulation variables
+
         self.__current_timestamp = 0.0
 
         self.__current_service = None
+
+        self.__event_q: list[Event] = []
+
+        self.__waiting_qs = list(
+            map(lambda _: list(), range(0, self.__number_of_qs)))
 
     #get an arrival time, given the poisson distribution(exponential inter arrivals)
     def __get_arrival_time(self):
@@ -50,17 +62,27 @@ class Simulator:
 
     #remove um evento na fila de eventos
     def __dequeue_event(self) -> Event:
-        event = self.__event_q[0]
+        event = self.__event_q.pop(0)
 
-        #adicionando no histórico de eventos
-        self.__event_q_history.append(event)
-
-        self.__event_q.pop(0)
+        self.__event_list_raw.append({
+            'reference': 'event',
+            **event.as_dict(), 
+            'round': self.__current_round
+        })
 
         return event
 
     def __dequeue_from_waiting_q(self, queue_number: int):
         next_client = self.__waiting_qs[queue_number - 1].pop(0)
+
+        self.__event_list_raw.append({
+            'reference': 'dequeue',
+            'queue_number': queue_number,
+            'timestamp': self.__current_timestamp,
+            'client_id': next_client[0],
+            'remaining_service_time': next_client[1],
+            'round': self.__current_round
+        })
 
         return next_client
 
@@ -69,17 +91,31 @@ class Simulator:
                                queue_number: int,
                                remaining_service_time: float = None,
                                in_the_front=False):
+
+        self.__event_list_raw.append({
+            'reference': 'enqueue',
+            'queue_number': queue_number,
+            'timestamp': self.__current_timestamp,
+            'client_id': client_id,
+            'remaining_service_time': remaining_service_time,
+            'in_the_front': in_the_front,
+            'round': self.__current_round
+        })
+
         if (in_the_front):
-            self.__waiting_qs[queue_number - 1].insert(0, (client_id, remaining_service_time))
+            self.__waiting_qs[queue_number - 1].insert(
+                0, (client_id, remaining_service_time))
         else:
-            self.__waiting_qs[queue_number - 1].append((client_id, remaining_service_time))
+            self.__waiting_qs[queue_number - 1].append(
+                (client_id, remaining_service_time))
 
     def __remove_current_service_departure(self):
         current_client_id, _ = self.__get_current_service()
 
-        departures_current_service = list(filter(lambda x: x.type == EventType.DEPARTURE
-                        and x.client_id == current_client_id
-                        and x.queue_number == 2, self.__event_q))
+        departures_current_service = list(
+            filter(
+                lambda x: x.type == EventType.DEPARTURE and x.client_id ==
+                current_client_id and x.queue_number == 2, self.__event_q))
 
         self.__event_q.remove(departures_current_service[0])
 
@@ -101,29 +137,31 @@ class Simulator:
         end_of_current_service = departures_current_service[0].timestamp
 
         return end_of_current_service
-    
+
     def __get_current_service(self):
-        if(self.__current_service == None):
+        if (self.__current_service == None):
             return None, None
 
         return self.__current_service
-    
-    def __set_current_service(self, client_id: int, source_event_type: EventType):
+
+    def __set_current_service(self, client_id: int,
+                              source_event_type: EventType):
         self.__current_service = (client_id, source_event_type)
 
     def __schedule_next_arrival(self):
-            next_arrival_time = \
-                self.__current_timestamp + self.__get_arrival_time()
+        next_arrival_time = \
+            self.__current_timestamp + self.__get_arrival_time()
 
-            if (next_arrival_time <= self.__system_max_arrival_time):
-                self.__enqueue_event(
-                    Event(timestamp=next_arrival_time,
-                          type=EventType.ARRIVAL,
-                          queue_number=1))
+        if (next_arrival_time <= self.__system_max_arrival_time):
+            self.__enqueue_event(
+                Event(timestamp=next_arrival_time,
+                      type=EventType.ARRIVAL,
+                      queue_number=1))
 
     def __handle_arrival(self, event: Event):
 
-        current_client_id, current_source_event_type = self.__get_current_service()
+        current_client_id, current_source_event_type = self.__get_current_service(
+        )
 
         if (current_client_id == None):
             next_client = Event(timestamp=self.__current_timestamp,
@@ -139,20 +177,20 @@ class Simulator:
         else:
             self.__enqueue_in_waiting_q(event.client_id, 1)
 
-            if(self.__get_waiting_q_size(1) == 1):
+            if (self.__get_waiting_q_size(1) == 1):
                 next_client = Event(timestamp=self.__current_timestamp,
-                                type=EventType.START_SERVICE_1,
-                                queue_number=1,
-                                client_id=event.client_id)
+                                    type=EventType.START_SERVICE_1,
+                                    queue_number=1,
+                                    client_id=event.client_id)
 
                 self.__enqueue_event(next_client, True)
 
                 current_client_id, _ = self.__get_current_service()
 
                 halt_service = Event(timestamp=self.__current_timestamp,
-                                    type=EventType.HALT_SERVICE_2,
-                                    queue_number=2,
-                                    client_id=current_client_id)
+                                     type=EventType.HALT_SERVICE_2,
+                                     queue_number=2,
+                                     client_id=current_client_id)
 
                 self.__enqueue_event(halt_service, True)
 
@@ -172,9 +210,9 @@ class Simulator:
 
     def __handle_end_service_1(self, event: Event):
         next_event = Event(timestamp=self.__current_timestamp,
-                            type=EventType.START_SERVICE_2,
-                            queue_number=2,
-                            client_id=event.client_id)
+                           type=EventType.START_SERVICE_2,
+                           queue_number=2,
+                           client_id=event.client_id)
 
         self.__enqueue_in_waiting_q(event.client_id, 2)
         self.__set_current_service(None, None)
@@ -185,18 +223,25 @@ class Simulator:
         elif (self.__get_waiting_q_size(1) > 0):
             next_client_id, _ = self.__get_next_client_in_waiting_q(1)
 
-            next_event = Event(client_id=next_client_id, type=EventType.START_SERVICE_1, timestamp=self.__current_timestamp, queue_number=1)
+            next_event = Event(client_id=next_client_id,
+                               type=EventType.START_SERVICE_1,
+                               timestamp=self.__current_timestamp,
+                               queue_number=1)
 
             self.__enqueue_event(next_event, True)
         elif (self.__get_waiting_q_size(2) > 0):
             next_client_id, _ = self.__get_next_client_in_waiting_q(2)
 
-            next_event = Event(client_id=next_client_id, type=EventType.START_SERVICE_2, timestamp=self.__current_timestamp, queue_number=2)
+            next_event = Event(client_id=next_client_id,
+                               type=EventType.START_SERVICE_2,
+                               timestamp=self.__current_timestamp,
+                               queue_number=2)
 
             self.__enqueue_event(next_event, True)
 
     def __handle_start_service_2(self, event: Event):
-        next_client, remaining_service_time = self.__dequeue_from_waiting_q(event.queue_number)
+        next_client, remaining_service_time = self.__dequeue_from_waiting_q(
+            event.queue_number)
 
         self.__set_current_service(next_client, event.type)
 
@@ -221,12 +266,16 @@ class Simulator:
         remaining_service_time = \
                     self.__get_end_of_current_service() - self.__current_timestamp
 
-        if (not(remaining_service_time > 0.0)):
+        if (not (remaining_service_time > 0.0)):
             remaining_service_time = None
 
         self.__remove_current_service_departure()
 
-        self.__enqueue_in_waiting_q(client_id, 2, in_the_front=True, remaining_service_time=remaining_service_time)
+        self.__enqueue_in_waiting_q(
+            client_id,
+            2,
+            in_the_front=True,
+            remaining_service_time=remaining_service_time)
 
         self.__set_current_service(None, None)
 
@@ -235,13 +284,19 @@ class Simulator:
         if (self.__get_waiting_q_size(1) > 0):
             next_client_id, _ = self.__get_next_client_in_waiting_q(1)
 
-            next_event = Event(client_id=next_client_id, type=EventType.START_SERVICE_1, timestamp=self.__current_timestamp, queue_number=1)
+            next_event = Event(client_id=next_client_id,
+                               type=EventType.START_SERVICE_1,
+                               timestamp=self.__current_timestamp,
+                               queue_number=1)
 
             self.__enqueue_event(next_event, in_the_front=True)
         elif (self.__get_waiting_q_size(2) > 0):
             next_client_id, _ = self.__get_next_client_in_waiting_q(2)
 
-            next_event = Event(client_id=next_client_id, type=EventType.START_SERVICE_2, timestamp=self.__current_timestamp, queue_number=2)
+            next_event = Event(client_id=next_client_id,
+                               type=EventType.START_SERVICE_2,
+                               timestamp=self.__current_timestamp,
+                               queue_number=2)
 
             self.__enqueue_event(next_event, in_the_front=True)
 
@@ -249,46 +304,57 @@ class Simulator:
 
         self.__schedule_next_arrival()
 
+    def __save_event_list_raw(self):
+        if (os.path.exists(self.__event_list_raw_file)):
+            os.remove(self.__event_list_raw_file)
+
+        if (len(self.__event_list_raw) > 0):
+            with open(self.__event_list_raw_file, 'a+',
+                      newline='') as output_file:
+
+                fieldnames = [
+                    'reference', 'type', 'queue_number', 'timestamp',
+                    'client_id', 'remaining_service_time', 'in_the_front',
+                    'round'
+                ]
+
+                dict_writer = csv.DictWriter(output_file,
+                                             fieldnames=fieldnames)
+                dict_writer.writeheader()
+                dict_writer.writerows(self.__event_list_raw)
+
     def run(self):
-        #enfileirando a primeira chegada na fila 1
-        self.__enqueue_event(
-            Event(timestamp=self.__current_timestamp +
-                  self.__get_arrival_time(),
-                  type=EventType.ARRIVAL,
-                  queue_number=1))
-
         try:
-            while (len(self.__event_q) > 0):
-                event: Event = self.__dequeue_event()
+            for round_number in range(0, self.__number_of_rounds):
+                self.__reset_simulation_variables()
 
-                self.__current_timestamp = event.timestamp
+                #enfileirando a primeira chegada na fila 1
+                self.__enqueue_event(
+                    Event(timestamp=self.__current_timestamp +
+                        self.__get_arrival_time(),
+                        type=EventType.ARRIVAL,
+                        queue_number=1))
 
-                print(event)
+                self.__current_round = round_number
 
-                if (event.type == EventType.ARRIVAL):
-                    self.__handle_arrival(event)
-                elif (event.type == EventType.START_SERVICE_1):
-                    self.__handle_start_service_1(event)
-                elif (event.type == EventType.END_SERVICE_1):
-                    self.__handle_end_service_1(event)
-                elif (event.type == EventType.START_SERVICE_2):
-                    self.__handle_start_service_2(event)
-                elif (event.type == EventType.HALT_SERVICE_2):
-                    self.__handle_halt_service_2(event)
-                elif (event.type == EventType.DEPARTURE):
-                    self.__handle_departure(event)
+                while (len(self.__event_q) > 0):
+                    event: Event = self.__dequeue_event()
+
+                    self.__current_timestamp = event.timestamp
+
+                    #print(event)
+
+                    if (event.type == EventType.ARRIVAL):
+                        self.__handle_arrival(event)
+                    elif (event.type == EventType.START_SERVICE_1):
+                        self.__handle_start_service_1(event)
+                    elif (event.type == EventType.END_SERVICE_1):
+                        self.__handle_end_service_1(event)
+                    elif (event.type == EventType.START_SERVICE_2):
+                        self.__handle_start_service_2(event)
+                    elif (event.type == EventType.HALT_SERVICE_2):
+                        self.__handle_halt_service_2(event)
+                    elif (event.type == EventType.DEPARTURE):
+                        self.__handle_departure(event)
         finally:
-            pass
-            # if(os.path.exists(self.__q_events_history_file)):
-            #     os.remove(self.__q_events_history_file)
-
-            # if(len(self.__q_events_history) > 0):
-            #         self.__save_event_history(self.__q_events_history, self.__q_events_history_file)
-            #         self.__q_events_history.clear()
-
-            # if(os.path.exists(self.__service_events_history_file)):
-            #     os.remove(self.__service_events_history_file)
-
-            # if(len(self.__service_events_history) > 0):
-            #         self.__save_event_history(self.__service_events_history, self.__service_events_history_file)
-            #         self.__service_events_history.clear()
+            self.__save_event_list_raw()
